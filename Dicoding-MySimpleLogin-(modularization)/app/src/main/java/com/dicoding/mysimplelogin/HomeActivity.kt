452031281298949fd2fcc013f9f0a0dd9d18d2f0
Abdullah Hali Saputra <abdullahhali060@gmail.com>
2024-02-03 19:12:45 +0700

    modularization dynamic feature

--- Dicoding-MySimpleLogin-(modularization)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ Dicoding-MySimpleLogin-(modularization)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -2,10 +2,13 @@ package com.dicoding.mysimplelogin
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.core.SessionManager
 import com.dicoding.core.UserRepository
 import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 
 class HomeActivity : AppCompatActivity() {
 
@@ -26,6 +29,39 @@ class HomeActivity : AppCompatActivity() {
             userRepository.logoutUser()
             moveToMainActivity()
         }
+
+        binding.fab.setOnClickListener {
+            try {
+                moveToChatActivity()
+            } catch (e: Exception) {
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun installChatModule() {
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val moduleChat = "chat"
+        if (splitInstallManager.installedModules.contains(moduleChat)) {
+            moveToChatActivity()
+            Toast.makeText(this, "Open Module", Toast.LENGTH_SHORT).show()
+        } else {
+            val request = SplitInstallRequest.newBuilder()
+                .addModule(moduleChat)
+                .build()
+            splitInstallManager.startInstall(request)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Success installing module", Toast.LENGTH_SHORT).show()
+                    moveToChatActivity()
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this, "Error installing module", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.dicoding.mysimplelogin.chat.ChatActivity")))
     }
 
     private fun moveToMainActivity() {
